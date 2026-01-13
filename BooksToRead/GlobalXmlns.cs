445123b3844f99d44e599c77767206c7f639da2c
Santiago Sica <santisica29@gmail.com>
2026-01-13 15:44:30 -0300

    Add project files.

---
+++ BooksToRead/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BooksToRead")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BooksToRead.Pages")]
---
+++ BooksToRead/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BooksToRead
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
